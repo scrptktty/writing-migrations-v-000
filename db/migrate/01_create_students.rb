c7d9cc868520503afa43fa371937b6a84e7eba8b
Kachel Wittig <kjwittig@gmail.com>
2018-06-26 18:25:37 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -3,9 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
 def change
     create_table :students do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
     end
   end
 end
