5acc7f728d2cd8c0008d9eb3a56315bc647107b0
Kachel Wittig <kjwittig@gmail.com>
2018-06-26 18:44:46 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
 
   def change
+    remove_column :students, :birthdate, :string
     add_column :students, :birthdate, :datetime
   end
 
